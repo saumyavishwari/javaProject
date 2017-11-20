d026e4fc77bea5c852b8a4d4da96983c8468b181
saumya <saumya@latitudefintech.com>
2017-11-20 12:54:40 +0530

    added peintNumber program

---
+++ printNumber.java
@@ -0,0 +1,11 @@
+public class printNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1; i<10; i++){
+			System.out.println("hello World...."+i);
+		}
+	}
+
+}
